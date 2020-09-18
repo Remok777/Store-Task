8b29d9d7db8056f0c9aab30dcd2225c67fd3a4a9
Remok777 <roman.sadokha01@gmail.com>
2020-09-18 19:22:19 +0300

    Adding project files

---
+++ Store/GoodsLib.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Store
+{
+    public abstract class Goods
+    {
+        private string _id;
+        private string _name;
+        private int _price;
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        
+
+    }
+    public class Laptop : Goods 
+    {
+        private int _ram;
+        private string _proc;
+        public int Ram
+        {
+            get { return _ram; }
+            set { _ram = value; }
+        }
+        public string Processor
+        {
+            get { return _proc; }
+            set { _proc = value; }
+        }
+
+
+    }
+    class Smartphone : Goods
+    {
+        private string _cam;
+        private string _screen;
+        public string Camera
+        {
+            get { return _cam; }
+            set { _cam = value; }
+        }
+        public string Screen
+        {
+            get { return _screen; }
+            set { _screen = value; }
+        }
+        
+    }
+
+}
---
+++ Store/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Laptop> Laptops = new List<Laptop>();
+            Laptops.Add(new Laptop { Id = "1", Name = "HP Pavilion Gaming", Price = 900, Ram = 8, Processor = "Intel Core i5-8300H" });
+            Laptops.Add(new Laptop { Id = "2", Name = "Asus X509FL-BQ293", Price = 550, Ram = 88, Processor = "Intel Core i5-8265U" });
+            Laptops.Add(new Laptop { Id = "3", Name = "Apple MacBook Air", Price = 1200, Ram = 88, Processor = "Intel Core i3" });
+
+            List<Smartphone> Smartphones = new List<Smartphone>();
+            Smartphones.Add(new Smartphone{Id = "1", Name = "Apple iPhone 6s", Price = 500, Camera = "8MP", Screen = "14``"});
+            Smartphones.Add(new Smartphone{Id = "2", Name = "Apple iPhone X", Price = 960, Camera = "24MP", Screen = "18``"});
+            Smartphones.Add(new Smartphone{Id = "3", Name = "Xiaomi Mi A2", Price = 300, Camera = "12MP", Screen = "15``"});
+            Smartphones.Add(new Smartphone{Id = "4", Name = "Huawei 12X", Price = 450, Camera = "13MP", Screen = "16``"});
+
+            LaptopSelection<Laptop> select1 = new LaptopSelection<Laptop>();
+
+            select1.SelectType(Laptops);
+            string id = "0";
+            Console.WriteLine("     Choose the type of electronics:");
+            Console.WriteLine("1 - Laptops");
+            Console.WriteLine("2 - Smartphones");
+            Console.Write("1 or 2: ");
+            id = Console.ReadLine();
+            if (id == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("                       Laptops");
+
+                foreach (var laptop in Laptops)
+                {
+                    Console.WriteLine($"{laptop.Id} | {laptop.Name} | {laptop.Price} | {laptop.Ram} | {laptop.Processor} |");
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("                    Smartphones");
+
+                foreach (var smartphone in Smartphones)
+                {
+                    Console.WriteLine($"{smartphone.Id} | {smartphone.Name} | {smartphone.Price} | {smartphone.Camera} | {smartphone.Screen} |");
+                }
+            }
+        }
+    }
+}
